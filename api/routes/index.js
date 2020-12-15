b465bf2d5d94df385a212e78121db452aee8ebc4
Kourtney Huget <kourtneyhuget@hotmail.com>
2020-12-15 10:49:22 -0700

    install pg package

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
+
+// module.exports = router;
+
+const express = require("express");
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
 });
 
-module.exports = router;
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
