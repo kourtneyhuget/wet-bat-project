8f31512b3bc27d6ee7aede155e3b9c969ade6c8d
Kourtney Huget <kourtneyhuget@hotmail.com>
2020-12-15 09:53:43 -0700

    test react router

---
+++ client/src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+function MainPage() {
+  return (
+    <div className="main-page-container">
+      <p>HELLO, WORLD</p>
+    </div>
+  );
+}
+
+export default MainPage;
