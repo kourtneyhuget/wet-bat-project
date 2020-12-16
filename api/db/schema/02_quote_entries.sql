b79ac80ad3ac7374b8e5a482fd8545a579517014
Kourtney Huget <kourtneyhuget@hotmail.com>
2020-12-15 17:03:20 -0700

    fix schema/seed naming convention

--- api/db/schema/02_quote_entries.sql
+++ api/db/schema/02_quote_entries.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS quote_entries CASCADE;
 CREATE TABLE quote_entries (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  departure TEXT NOT NULL,
-  destination TEXT NOT NULL,
-  depart DATE NOT NULL,
-  return DATE NOT NULL,
+  departure_location TEXT NOT NULL,
+  destination_location TEXT NOT NULL,
+  depart_date DATE NOT NULL,
+  return_date DATE NOT NULL,
   people NUMERIC NOT NULL,
   transportation TEXT NOT NULL,
   name TEXT NOT NULL,
-  phone BIGINT NOT NULL,
-  price INTEGER
+  phone_number BIGINT NOT NULL,
+  price NUMERIC DEFAULT 1000
 );
