99a99f1ab9186f20a5d0529e143a46b1b75b5bbd
Kourtney Huget <kourtneyhuget@hotmail.com>
2020-12-18 16:44:51 -0700

    add detailed quotes route to display detailed quotes

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,16 @@ import React from "react";
 // import "./App.css";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MainPage } from "./components/MainPage";
+import { DetailedQuotes } from "./components/DetailedQuotes";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/detailedquotes" component={DetailedQuotes} />
+      </div>
+    </Router>
   );
 }
---
+++ client/src/components/DetailedQuotes.jsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import TableFooter from "@material-ui/core/TableFooter";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import axios from "axios";
+
+const useStyles = makeStyles(() => ({
+  container: {
+    maxHeight: 550,
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#5BBFBA",
+    },
+    secondary: {
+      main: "#5F6CAF",
+    },
+  },
+});
+
+export function DetailedQuotes(props) {
+  const classes = useStyles();
+  const [pending, setPending] = useState([]);
+  const [selectedId, setSelectedId] = useState([]);
+
+  const handleChange = (event) => {
+    setSelectedId(event.target.value);
+  };
+
+  const getPending = async (event) => {
+    try {
+      const response = await fetch("/api/pending");
+      const jsonData = await response.json();
+      setPending(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const moveTask = async (quoteId) => {
+    axios
+      .put(`api/quotescompleted/${quoteId}`, {
+        selectedId: quoteId,
+      })
+      .then((res) => {
+        console.log("MADE IT TO THE BACKEND");
+        setPending(pending.filter((quote) => quote.id !== quoteId));
+        props.updateCompletedQuotes();
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+  };
+
+  useEffect(() => {
+    getPending();
+  }, []);
+
+  return (
+    <div className="pending-quote-container">
+      <ThemeProvider theme={theme}>
+        <TableContainer className={classes.container} component={Paper}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableCell
+                align="center"
+                colSpan={11}
+                style={{ color: "#5BBFBA" }}
+              >
+                <span id="title">PENDING QUOTES</span>
+              </TableCell>
+              <TableRow>
+                {/* <TableCell style={{ color: "#F0CF85" }}>ID</TableCell> */}
+                <TableCell style={{ color: "#F0CF85" }}>NAME</TableCell>
+                <TableCell></TableCell>
+                <TableCell style={{ color: "#F0CF85" }}>PHONE</TableCell>
+                {/* <TableCell style={{ color: "#F0CF85" }}>FROM</TableCell> */}
+                <TableCell style={{ color: "#F0CF85" }}>TO</TableCell>
+                <TableCell style={{ color: "#F0CF85" }}>DEPART</TableCell>
+                {/* <TableCell style={{ color: "#F0CF85" }}>RETURN</TableCell> */}
+                <TableCell style={{ color: "#F0CF85" }}>PRICE</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {pending.map((quote, index) => (
+                <TableRow key={quote.id}>
+                  <TableCell component="th" scope="row">
+                    {quote.first_name}
+                  </TableCell>
+                  {/* <TableCell>{quote.first_name}</TableCell> */}
+                  <TableCell>{quote.last_name}</TableCell>
+                  <TableCell>{quote.phone_number}</TableCell>
+                  {/* <TableCell>{quote.departure_location}</TableCell> */}
+                  <TableCell>{quote.destination_location}</TableCell>
+                  <TableCell>{quote.depart_date.slice(0, 10)}</TableCell>
+                  {/* <TableCell>{quote.return_date.slice(0, 10)}</TableCell> */}
+                  <TableCell>${quote.price}</TableCell>
+                  <TableCell>
+                    <Button
+                      onClick={() => moveTask(quote.id)}
+                      onChange={handleChange}
+                      variant="contained"
+                      size="large"
+                      color="primary"
+                      type="submit"
+                    >
+                      CLOSE
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell className="icons-style"></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </ThemeProvider>
+    </div>
+  );
+}
